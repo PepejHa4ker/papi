0897013ce415c8bc60b826f55a7ab4890f2c9156
pepej <semenovich-nikitka@mail.ru>
2021-06-11 11:43:25 +1000

    Implemented ConditionalValue

---
+++ papi-core/src/main/java/com/pepej/papi/metadata/ConditionalValue.java
@@ -0,0 +1,39 @@
+package com.pepej.papi.metadata;
+
+import org.checkerframework.checker.nullness.qual.Nullable;
+
+import java.util.Objects;
+import java.util.function.Predicate;
+
+public class ConditionalValue<T> implements TransientValue<T> {
+
+
+    private final Predicate<? super T> expireCondition;
+    private final T value;
+
+    public static <T, E> ConditionalValue<T> of(T value, Predicate<? super T> expireCondition) {
+        Objects.requireNonNull(value, "value");
+        Objects.requireNonNull(expireCondition, "expireCondition");
+        return new ConditionalValue<>(value, expireCondition);
+    }
+
+    private ConditionalValue(T value, Predicate<? super T> expireCondition) {
+        this.value = value;
+        this.expireCondition = expireCondition;
+    }
+
+    @Nullable
+    @Override
+    public T getOrNull() {
+        if (shouldExpire()) {
+            return null;
+        }
+
+        return this.value;
+    }
+
+    @Override
+    public boolean shouldExpire() {
+        return expireCondition.test(value);
+    }
+}
